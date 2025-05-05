214485a2c27d7a765af0a0d325632302669876fe
Umra-Badar20 <umrabadar555@gmail.com>
2025-05-05 17:24:29 +0500

    api added

--- frontend/admin/admin.js
+++ frontend/admin/admin.js
@@ -34,7 +34,7 @@ createApp({
   methods: {
     async login() {
       try {
-        const response = await axios.post('https://croxton-consultant-limited.vercel.app/admin/index.html', this.loginData);
+        const response = await axios.post('https://croxton-consultant-limited-production.up.railway.app/api/auth/login', this.loginData);
         this.token = response.data.token;
         localStorage.setItem('token', this.token);
         this.authenticated = true;
